3cb3b4c12e8afc5fc7c20e022474d69fe48cd8b3
parag <pal.parag257@gmail.com>
2021-03-17 14:51:12 +0530

    Updated IRremote lib

--- IR-Sensor/IR-Sensor.ino
+++ IR-Sensor/IR-Sensor.ino
@@ -1,19 +1,18 @@
-#include <IRremote.h>
+#include <IRremote.h> // include the IRremote library
 
-IRrecv IR(9);
-decode_results out;
+#define RECEIVER_PIN 2 // define the IR receiver pin
+IRrecv receiver(RECEIVER_PIN); // create a receiver object of the IRrecv class
+decode_results results; // create a results object of the decode_results class
 
 void setup() {
-  Serial.begin(9600);
-  IR.enableIRIn();
+  Serial.begin(9600); // begin serial communication with a baud rate of 9600
+  receiver.enableIRIn(); // enable the receiver
 }
-void loop() {
-  // put your main code here, to run repeatedly:
 
-  if (IR.decode()) {
-    Serial.println(out.value, HEX);
-    Serial.print("  Got it");
-    delay(1500);
-    IR.resume();
+void loop() {
+  if (receiver.decode(&results)) {
+    Serial.println(results.value, HEX);
+    Serial.println("yo");
+    receiver.resume();
   }
 }
