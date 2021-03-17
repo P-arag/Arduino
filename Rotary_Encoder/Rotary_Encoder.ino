64f5a730e26f15599330304b69cd9b7220f93902
parag <pal.parag257@gmail.com>
2021-03-17 15:13:21 +0530

    Rotary Encoder

--- Rotary_Encoder/Rotary_Encoder.ino
+++ Rotary_Encoder/Rotary_Encoder.ino
@@ -1,9 +1,31 @@
+#define outputA 6
+#define outputB 7
+#define switchPin 12
+
+int counter = 0;
+int aState;
+int aLastState;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(outputA, INPUT);
+  pinMode(outputB, INPUT);
+  pinMode(switchPin, INPUT);
+
+  Serial.begin(9600);
+  aLastState = digitalRead(outputA);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  aState = digitalRead(outputA); 
+  if (aState != aLastState) {
+    if (digitalRead(outputB) != aState) {
+      counter ++;
+    } else {
+      counter --;
+    }
 
+    Serial.println("Position: " + String(counter));
+  }
+  aLastState = aState;  
 }
